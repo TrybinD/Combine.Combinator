c305bcfcb0618ae4854907c6ed4d1d9feec52898
Trubin <trybin420@gmail.com>
2024-03-03 18:17:20 +0300

    fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = create_app()
 
 if __name__ == "__main__":
 
-    host = os.getenv("HOST", "127.0.0.1")
+    host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "7000"))
 
     uvicorn.run(app, host=host, port=port)
